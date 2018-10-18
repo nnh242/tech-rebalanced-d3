027dcac003aa65c7c2a7fa7948683e2bfec67931
Jessica Dembe <jmdembe@gmail.com>
2018-10-17 22:40:11 -0400

    added data for bar charts

---
+++ START/main.js
@@ -0,0 +1,9 @@
+const data = [
+    { day: 'Sunday', temp: 52 },
+    { day: 'Monday', temp: 56 }, 
+    { day: 'Tuesday', temp: 62}, 
+    { day: 'Wednesday', temp: 55},
+    { day: 'Thursday', temp: 52},
+    { day: 'Friday', temp: 57},
+    { day: 'Saturday', temp: 61}
+]
