c6c2e9209d480ddcde4a4346b34c99dcd7b22ca7
Jessica Dembe <jessica.dembe@associates.hq.dhs.gov>
2018-10-19 23:21:12 -0400

    refactored html and code solution

--- FINISH/main.js
+++ FINISH/main.js
@@ -6,4 +6,11 @@ const data = [
     { day: 'Thursday', temp: 52 },
     { day: 'Friday', temp: 57 },
     { day: 'Saturday', temp: 61 }
-]
+];
+
+const margin = { top: 5, right: 5, bottom: 50, left: 50};
+const height = 960;
+const width = 500;
+
+d3.select("body").append("svg").attr('height', 960).attr('width', 500);
+const svg = d3.select('svg');
