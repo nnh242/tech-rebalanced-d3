c40b1bdde0f65e485bf01f7050455dfdbb4a8622
Jessica Dembe <jessica.dembe@associates.hq.dhs.gov>
2018-10-20 11:48:24 -0400

    pushing draft of finished weather bar chart

--- FINISH/main.js
+++ FINISH/main.js
@@ -14,3 +14,37 @@ const width = 500;
 
 d3.select("body").append("svg").attr('height', 960).attr('width', 500);
 const svg = d3.select('svg');
+
+let x = d3.scaleBand().rangeRound([0, width]).padding(0.1);
+let y = d3.scaleLinear().rangeRound([height, 0]);
+
+let g = svg.append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+
+x.domain(data.map(function(d) { return d.day; }));
+  y.domain([0, d3.max(data, function(d) { return d.temp; })]);
+
+  g.append("g")
+      .attr("class", "axis axis--x")
+      .attr("transform", "translate(0," + height + ")")
+      .call(d3.axisBottom(x));
+
+  g.append("g")
+      .attr("class", "axis axis--y")
+      .call(d3.axisLeft(y).ticks(10, "%"))
+    .append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 6)
+      .attr("dy", "0.71em")
+      .attr("text-anchor", "end")
+      .text("Frequency");
+
+  g.selectAll(".bar")
+    .data(data)
+    .enter().append("rect")
+      .attr("class", "bar")
+      .attr("x", function(d) { return x(d.day); })
+      .attr("y", function(d) { return y(d.)temp; })
+      .attr("width", x.bandwidth())
+      .attr("height", function(d) { return height - y(d.temp); });
